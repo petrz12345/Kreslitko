042727fc2cc016dfa04bcd851e6e47b7581e89b3
pleslea20v-ops <pleslea20v@gyri.cz>
2026-05-05 13:30:05 +0200

    Přidejte soubory projektu.

---
+++ Kreslitko/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Kreslitko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            this.btDraw = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.BackColor = System.Drawing.Color.White;
+            this.pbCanvas.Location = new System.Drawing.Point(13, 13);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(775, 425);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
+            // 
+            // btDraw
+            // 
+            this.btDraw.Location = new System.Drawing.Point(42, 464);
+            this.btDraw.Name = "btDraw";
+            this.btDraw.Size = new System.Drawing.Size(75, 23);
+            this.btDraw.TabIndex = 1;
+            this.btDraw.Text = "Draw";
+            this.btDraw.UseVisualStyleBackColor = true;
+            this.btDraw.Click += new System.EventHandler(this.btDraw_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(159, 464);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(235, 20);
+            this.txtDisplay.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 519);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btDraw);
+            this.Controls.Add(this.pbCanvas);
+            this.Name = "Form1";
+            this.Text = "ProgramDraw";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCanvas;
+        private System.Windows.Forms.Button btDraw;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtDisplay;
+    }
+}
+
---
+++ Kreslitko/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kreslitko
+{
+    
+    /*public void DrawStickman(Graphics g, Pen pen, int x, int y)
+        {
+            g.DrawEllipse(pen, x + 100, y + 100, 50, 50);
+            g.DrawLine(pen, x + 125, y + 150, x + 125, y + 250);
+            g.DrawLine(pen, x + 125, y + 250, x + 100, y + 300);
+            g.DrawLine(pen, x + 125, y + 250, x + 150, y + 300);
+            g.DrawLine(pen, x + 125, y + 160, x + 100, y + 200);
+            g.DrawLine(pen, x + 125, y + 160, x + 150, y + 200);
+        }*/
+    public partial class Form1 : Form
+    {
+        //drawing library
+        Graphics mobjGraphics;
+        //konstrucktor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //form loading
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //připojit grafiku na obrázkovou krabici
+            mobjGraphics = pbCanvas.CreateGraphics();
+        }
+
+        private void btDraw_Click(object sender, EventArgs e)
+        {
+            Pen MyPen = new Pen(Color.Red); MyPen.Width = 4;
+
+            //DrawStickman(mobjGraphics, MyPen);
+
+
+            
+            mobjGraphics.DrawEllipse(MyPen, 100, 100, 50, 50);
+            mobjGraphics.DrawLine(MyPen, 125, 150, 125, 250);
+            mobjGraphics.DrawLine(MyPen, 125, 250, 100, 300);
+            mobjGraphics.DrawLine(MyPen, 125, 250, 150, 300);
+            mobjGraphics.DrawLine(MyPen, 125, 160, 100, 200);
+            mobjGraphics.DrawLine(MyPen, 125, 160, 150, 200);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtDisplay.Text = DateTime.Now.ToString();
+        }
+    }
+}
